3d842a9b8e95090ab1314024bcefaa3d5a767f4a
Dharmik Gadhiya <dharmikgadhiya52525@gmail.com>
2024-10-03 01:55:12 +0530

    First commit

---
+++ assest/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  // Get the element
+  var box = document.getElementById("main");
+  var box2 = document.getElementById("name");
+  var box3 = document.getElementById("first");
+  var box4 = document.getElementById("second");
+
+  // Add the class to trigger the animation
+  box.classList.add("animate-loaded");
+  box2.classList.add("animate-loaded-name");
+  box3.classList.add("first-technology-name-animation");
+  box4.classList.add("second-technology-name-animation");
+};
